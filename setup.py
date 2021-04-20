a51f48c10bc17b61c8fdc343ef14749bd373481f
Enrico La Sala <enrico.lasala@buildnn.com>
2021-04-20 10:08:25 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project for the Data Science course of Unipa',
+    author='Enrico La Sala',
+    license='',
+)
